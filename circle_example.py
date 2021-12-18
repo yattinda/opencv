600128a53960717a7a8bc922e49e7a387d08f528
yattinda <yattinda@gmail.com>
2021-12-18 16:18:47 +0900

    Binarization

--- circle_example.py
+++ circle_example.py
@@ -4,10 +4,19 @@ import numpy as np
 def find_circle(cv2_img, param, last_len):
   gray = cv2.cvtColor(cv2_img, cv2.COLOR_BGR2GRAY)
   retval, bw = cv2.threshold(gray, 130, 255, cv2.THRESH_BINARY)
+  bw_floodfill = bw.copy()
+
+  h, w = bw.shape[:2]
+  mask = np.zeros((h+2, w+2), np.uint8)
+
+  cv2.floodFill(bw_floodfill, mask, (0,0), 255);
+  bw_floodfill_inv = cv2.bitwise_not(bw_floodfill)
+  kernel = np.ones((15, 15), np.uint8)
+  dilation_img = cv2.dilate(bw_floodfill_inv, kernel, iterations=1)
   while(1):
     print(last_len, param)
     print(type(gray))
-    circles = cv2.HoughCircles(bw, cv2.HOUGH_GRADIENT, dp=1, minDist=20, param1=100, param2=param, minRadius=0, maxRadius=0)
+    circles = cv2.HoughCircles(dilation_img, cv2.HOUGH_GRADIENT, dp=1, minDist=20, param1=100, param2=param, minRadius=0, maxRadius=0)
     if isinstance(circles, np.ndarray):
       if len(circles[0]) == 1:
         circles = np.uint16(np.around(circles))
