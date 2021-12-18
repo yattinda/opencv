2b28ef6a0cde471f1186f69082d3f944db72c01a
yattinda <yattinda@gmail.com>
2021-12-19 03:26:14 +0900

    Test

--- ditect_circle.py
+++ ditect_circle.py
@@ -2,6 +2,10 @@ import cv2
 import numpy as np
 import math
 
+def adjust(img, alpha=1.0, beta=0.0):
+    dst = alpha * img + beta
+    return np.clip(dst, 0, 255).astype(np.uint8)
+
 def accuracy_area(area, circumference):
     area = int(area)
     circumference = int(circumference)
@@ -30,13 +34,14 @@ def detect_contour(image_path):
     h, w = src.shape[:2]
     re_h = re_w = re_length / max(h,w)
     resize_src = cv2.resize(src, dsize=None, fx=re_h , fy=re_w)
-    gray = cv2.cvtColor(resize_src, cv2.COLOR_BGR2GRAY)
+    dst = adjust(resize_src, alpha=1.1, beta=0.0)
+    gray = cv2.cvtColor(dst, cv2.COLOR_BGR2GRAY)
     #2値化
     retval, bw = cv2.threshold(gray, 130, 255, cv2.THRESH_BINARY)
     bw_floodfill = bw.copy()
     h, w = bw.shape[:2]
     mask = np.zeros((h+2, w+2), np.uint8)
-    cv2.floodFill(bw_floodfill, mask, (0,0), 255);
+    cv2.floodFill(bw_floodfill, mask, (0,0), 255)
     bw_floodfill_inv = cv2.bitwise_not(bw_floodfill)
     kernel = np.ones((15, 15), np.uint8)
     dilation_img = cv2.dilate(bw_floodfill_inv, kernel, iterations=1)
@@ -80,10 +85,8 @@ def detect_contour(image_path):
 
         if mid_accuracy > 0.98:
             accuracy = mid_accuracy
-        elif max_accuracy - min_accuracy > 0.30 and min_accuracy < 0.5:
+        elif max_accuracy - min_accuracy > 0.30 and min_accuracy < 0.5 or sum_accuracy <= 1.8:
             accuracy = sum_accuracy / 3
-        if sum_accuracy <= 1.8:
-            accuracy = max_accuracy
     else:
         accuracy = 0
 
@@ -92,10 +95,11 @@ def detect_contour(image_path):
     print(accuracy_rad(radius, s_side))
     print("#######################")
     print(accuracy)
-
-    cv2.imshow("Inverted Floodfilled Image", bw_floodfill_inv)
+    cv2.imshow("bw", bw)
+    cv2.imshow("dst", dst)
+    # cv2.imshow("Inverted Floodfilled Image", bw_floodfill_inv)
     cv2.imshow("dilation_img", dilation_img)
     cv2.waitKey(0)
 
 if __name__ == '__main__':
-  detect_contour('image/circle5.jpg')
+  detect_contour('image/circle2.jpg')
